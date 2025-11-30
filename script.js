73f13a329055eafbb9a0fe55bcfa19ca4a829706
Yusuf Febrian Shiddiq <yusuffebrianshiddiq22@gmail.com>
2025-11-30 12:24:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Mengaktifkan library AOS (Animate On Scroll)
+AOS.init({
+    duration: 1000, // Durasi animasi (dalam milidetik)
+    once: true      // Animasi hanya terjadi sekali saat elemen muncul
+});
